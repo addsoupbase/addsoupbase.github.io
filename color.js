3cb5b1317f628a25536cf4414c71c2af301d6ef3
addsoupbase <162063292+addsoupbase@users.noreply.github.com>
2025-01-22 22:19:52 -0500

    color constructor

--- color.js
+++ color.js
@@ -19,8 +19,8 @@ class COLOR_MANAGER {
     }
     opposite(colour) {
         if (0 === colour.indexOf("#") && (colour = colour.slice(1)), 3 === colour.length && (colour = colour[0] + colour[0] + colour[1] + colour[1] + colour[2] + colour[2]), 6 !== colour.length) throw TypeError('Invalid HEX color.')
-        let f = (255 - parseInt(colour.slice(0, 2), 16)).toString(16), 
-            $ = (255 - parseInt(colour.slice(2, 4), 16)).toString(16), 
+        let f = (255 - parseInt(colour.slice(0, 2), 16)).toString(16),
+            $ = (255 - parseInt(colour.slice(2, 4), 16)).toString(16),
             a = (255 - parseInt(colour.slice(4, 6), 16)).toString(16)
         return `#${f.padStart(0, 2)}${$.padStart(0, 2)}${a.padStart(0, 2)}`
     }
@@ -32,5 +32,120 @@ class COLOR_MANAGER {
         new.target.#canvas = new OffscreenCanvas(0, 0).getContext('2d')
     }
 }
+export function Color(r = 255, g = 255, b = 255, a = 1) {
+    if (!new.target) return new Color(r, g, b, a)
+    if (typeof r === 'string' && (() => { try { color[r] } catch { return false }; return true })()) {
+        r = color[r]
+    }
+    if (typeof r !== 'string') { }
+    else if (typeof r === 'string' && r.startsWith('#')) {
+        let hex = r.slice(1)
+        if (hex.length === 3) hex = hex.split('').map(c => c + c).join('')
+        r = parseInt(hex.slice(0, 2), 16),
+            g = parseInt(hex.slice(2, 4), 16),
+            b = parseInt(hex.slice(4, 6), 16),
+            a = 1
+    }
+    else if (typeof r === 'string' && r.startsWith('rgb')) {
+        let [_, red, green, blue] = r.match(/\d+/g)
+        r = +red, g = +green, b = +blue, a = 1
+    }
+   /* else if (typeof r === 'string' && r.startsWith('hsl')) {
+        let [_, h, s, l] = r.match(/\d+/g)
+        r = +h, g = +s, b = +l, a = 1
+    }
+    else if (typeof r === 'string' && r.startsWith('hsv')) {
+        let [_, h, s, v] = r.match(/\d+/g)
+        r = +h, g = +s, b = +v, a = 1
+    }
+    else if (typeof r === 'string' && r.startsWith('lab')) {
+        let [_, l, a, b] = r.match(/\d+/g)
+        r = +l, g = +a, b = +b, a = 1
+    }*/
+    else if (typeof r === 'string' && r.startsWith('rgba')) {
+        let [_, red, green, blue, alpha] = r.match(/\d+/g)
+        r = +red, g = +green, b = +blue, a = +alpha
+    }
+   /* else if (typeof r === 'string' && r.startsWith('hsla')) {
+        let [_, h, s, l, alpha] = r.match(/\d+/g)
+        r = +h, g = +s, b = +l, a = +alpha
+    }
+    else if (typeof r === 'string' && r.startsWith('hsva')) {
+        let [_, h, s, v, alpha] = r.match(/\d+/g)
+        r = +h, g = +s, b = +v, a = +alpha
+    }*/
+    Object.assign(this, { r, g, b, a })
+}
+Object.defineProperties(Color.prototype, {
+    toString: {
+        value(format) {
+            switch (format) {
+                default: return `#${this.r.toString(16).padStart(2, 0)}${this.g.toString(16).padStart(2, 0)}${this.b.toString(16).padStart(2, 0)}`
+                case 'rgb': return `rgb(${this.r},${this.g},${this.b})`
+                case 'rgba': return `rgba(${this.r},${this.g},${this.b},${this.a})`
+                case 'hex2': return `#${this.r.toString(16).padStart(2, 0)}${this.g.toString(16).padStart(2, 0)}${this.b.toString(16).padStart(2, 0)}${this.a.toString(16).padStart(2, 0)}`
+                //case 'hsl': return `hsl(${this.r},${this.g}%,${this.b}%)`
+                //case 'hsla': return `hsla(${this.r},${this.g}%,${this.b}%,${this.a})`
+                //case 'hsv': return `hsv(${this.r},${this.g}%,${this.b}%)`
+                //case 'hsva': return `hsva(${this.r},${this.g}%,${this.b}%,${this.a})`
+                //case 'lab': return `lab(${this.r},${this.g},${this.b})`
+            }
+        }
+    },
+    flip: {
+        value() {
+            Object.assign(this, Color(color.opposite(this.toString())))
+        }
+    },
+    0: {
+        get() {
+            return this.r
+        },
+        set(r) {
+            this.r = r
+        }
+    },
+    1: {
+        get() {
+            return this.g
+        },
+        set(g) {
+            this.g = g
+        }
+    },
+    2: {
+        get() {
+            return this.b
+        },
+        set(b) {
+            this.b = b
+        }
+    },
+    3: {
+        get() {
+            return this.a
+        },
+        set(a) {
+            this.a = a
+        }
+    },
+    opacity: {
+        get() {
+            return this.a
+        },
+        set(opacity) {
+            this.a = opacity
+        }
+    },
+    [Symbol.iterator]: {
+        *value() {
+            yield this.r
+            yield this.g
+            yield this.b
+            yield this.a
+        }
+    }
+})
+window.c = Color
 const color = new Proxy(new COLOR_MANAGER, COLOR_MANAGER.handler)
 export default color
