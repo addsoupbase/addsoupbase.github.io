3b46e50fa381cbb7a76104cbaeb7cdcde2d4dc3f
addsoupbase <addsoupbase1@gmail.com>
2025-02-22 20:55:58 -0500

    whoops

--- silence.js
+++ silence.js
@@ -4,6 +4,6 @@ function nothing() { }
 if (!/localhost|127\.0\.0\.1/.test(location.host)) for (let i in console) console[i] = nothing
 console.print ?? Object.defineProperty(console, 'print', {
     value(...data) {
-        log(data.map(o => typeof o === 'object' ? JSON.stringify(o) : o))
+        log.apply(1, data.map(o => typeof o === 'object' ? JSON.stringify(o) : o))
     }
 })
