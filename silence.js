9f0fbe1e8ec6b69d6e2e28ce967922d30919f723
addsoupbase <addsoupbase1@gmail.com>
2025-03-03 14:45:55 -0500

    :)

--- silence.js
+++ silence.js
@@ -1,5 +1,6 @@
 // Since logging prevents objects from being garbage collected:
 void function () {
+    'use strict'
     if (!/localhost|127\.0\.0\.1/.test(location.host)) for (let i in console) {
         if (typeof console[i] !== 'function') continue
         let old = console[i]
@@ -8,7 +9,7 @@ void function () {
                 old.apply(console, data.map(o => o&&typeof o === 'object' ? ('outerHTML'in o ? o.outerHTML : JSON.stringify(o)) : o))
             }
             catch {
-                console[i](`[REDACTED]`)
+                old(`♻️ (Object was not logged to prevent a potential memory leak)`)
             }
         }
     }
