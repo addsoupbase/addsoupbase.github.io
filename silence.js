9e1172b03e9c1a5f012fed290a127aa0f301f85d
addsoupbase <addsoupbase1@gmail.com>
2025-03-03 13:37:38 -0500

    whoopps

--- silence.js
+++ silence.js
@@ -1,9 +1,11 @@
 // Since logging prevents objects from being garbage collected:
-const { log } = console
-function nothing() { }
+void function () { 
+    const { log } = console
+    function nothing() { }
 if (!/localhost|127\.0\.0\.1/.test(location.host)) for (let i in console) console[i] = nothing
 console.print ?? Object.defineProperty(console, 'print', {
     value(...data) {
         log.apply(1, data.map(o => typeof o === 'object' ? JSON.stringify(o) : o))
     }
 })
+}()
