79c671b7d419edb92d2e053f4fb18dd2da433423
addsoupbase <addsoupbase1@gmail.com>
2025-03-04 19:06:30 -0500

    minor fix²

--- csshelper.js
+++ csshelper.js
@@ -431,7 +431,7 @@ var allProps = [
         name: '--tap-highlight-color',
         inherits,
         syntax: '<color>',
-        initialValue: 'black'
+        initialValue: 'transparent'
     },
     {
         name: '--touch-callout',
