8ae3fed193ccee2521293935916e4f9fe8e6a73e
addsoupbase <addsoupbase1@gmail.com>
2025-03-22 21:00:53 -0400

    fix

--- silence.js
+++ silence.js
@@ -6,7 +6,7 @@ void function () {
         let old = console[i]
         console[i] = (...data) => {
             try {
-                old.apply(console, data.map(o => o&&typeof o === 'object'||typeof o === 'function' ? ('outerHTML'in o ? o.outerHTML : JSON.stringify(o)) : o))
+                old.apply(console, data.map(o => o&&(typeof o === 'object'||typeof o === 'function') ? ('outerHTML'in o ? o.outerHTML : JSON.stringify(o)) : `${o}`))
             }
             catch {
                 old(`♻️ (Object was not logged to prevent a potential memory leak)`)
