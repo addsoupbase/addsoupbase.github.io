d5439541fe592daffba6e9b3d775e56946d340a6
addsoupbase <addsoupbase1@gmail.com>
2025-06-19 14:37:52 -0400

    yeash

--- createEntry.js
+++ createEntry.js
@@ -1,8 +1,9 @@
 import { exists } from "jsr:@std/fs/exists"
 // deno run --watch --allow-read --allow-write createEntry.js
 function randomColor() {return `#${(Math.floor(Math.random() * 0x1000000)).toString(16).padStart(6, 0)}`}
+let slashes = /\//g
 let today = new Date
-    , now = today.toLocaleDateString('en-US').replace(/\//g, '_'),
+    , now = today.toLocaleDateString('en-US').replace(slashes, '_'),
     path = `./entries/${now}/`
 if (await exists(path)) throw Error(`Entry already exists for today (${now})`)
 let template = await Deno.readFile('./template.html')
@@ -12,6 +13,13 @@ let txt = decoder.decode(template)
     .replace(/ISO_DATE_TODAY/g, today.toISOString())
     .replace(/\.\/main\.png/g,`https://addsoupbase.github.io/entries/${now}/main.png`)
 .replace(/THEME_COLOR/g,randomColor())
+let yesterday = new Date
+    yesterday.setDate(yesterday.getDate() - 1)
+    let tomorrow = new Date
+        tomorrow.setDate(tomorrow.getDate() + 1)
+        txt = txt
+.replace(/DATE_FOR_YESTERDAY/g, yesterday.toLocaleDateString().replace(slashes, '_'))
+.replace(/DATE_FOR_TOMORROW/g, tomorrow.toLocaleDateString().replace(slashes, '_'))
     await Deno.mkdir(path)
 await Deno.writeTextFile(`${path}/index.html`, txt)
 console.log('Created entry for today!')
--- diary.js
+++ diary.js
@@ -1,2 +1,3 @@
 console.log('Welcome to my diary lol')
 // basically, if i want to run JS for every entry, it would go in this file
+if (document.prerendering) console.log(`Prerendering started for ${location}`)
