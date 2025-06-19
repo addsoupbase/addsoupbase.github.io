aa60a0e23275bd9e3d2b32797d77f7e7ae0c30c0
addsoupbase <addsoupbase1@gmail.com>
2025-06-18 23:22:28 -0400

    Rainbow carrot parking

--- silence.js
+++ silence.js
@@ -1,6 +1,7 @@
 // Since logging prevents objects from being garbage collected:
 typeof console !== 'undefined' && function () {
     'use strict'
+    var is = Array.isArray
     if (!/localhost|127\.0\.0\.1/.test(location.host)) {
         setInterval(console.clear, 180000)
         var ignore = /^(?:clear|count(?:Reset)?|createTask|profile(?:End)?|context|time(?:End|Stamp|Log)?)$/
@@ -14,7 +15,7 @@ typeof console !== 'undefined' && function () {
                                 if (typeof o === 'function') out = o.toString()
                                 else if (o && typeof o === 'object') {
                                 if ('outerHTML' in o) out = o.outerHTML
-                                else if (o.toString !== {}.toString) out = o.toString()
+                                else if (o.toString !== {}.toString && !is(o)) out = o.toString()
                                 else out = JSON.stringify(o) || o + ''
                             }
                             if (typeof out === 'string' && out.length > 500) out = out.slice(0, 500) + '…'
