a2ad4cffd8e46bb7e0a824a13ba88662f04fbbd9
addsoupbase <addsoupbase1@gmail.com>
2025-07-19 03:28:32 -0400

    okay

--- diary.js
+++ diary.js
@@ -1,58 +1,137 @@
-'use strict'
-console.log('Welcome to my diary lol')
-// basically, if i want to run JS for every entry, it would go in this file
+'use strict';
+// window.addEventListener('load', 
+(window.requestIdleCallback || setTimeout)(async function load() 
+{
+    console.log('Thing...')
+    let { default: $ } = await import('./yay.js')
+    // let observer = new MutationObserver(mutate)
+    let grammar = {
+        im: /(?=(\b|^))im(?=(\b|$))/g,
+        i: /(?=(\b|^))i(?=(\b|$))/g,
+        bc: /(?=(\b|^))bc(?=(\b|$))/ig,
+        idc: /(?=(\b|^))idc(?=(\b|$))/ig,
+        idk: /(?=(\b|^))idk(?=(\b|$))/ig,
+        ik: /(?=(\b|^))ik(?=(\b|$))/ig,
+        istg: /(?=(\b|^))istg(?=(\b|$))/ig,
+        ig: /(?=(\b|^))ig(?=(\b|$))/ig
+    }
+    /*
+    let end = /(?!([.!?-])).$/
+    function dot(str) {
+        return `${str}.`
+    }
+        */
+    let ignore = /A/i
+    $.body.treeWalker(NodeFilter.SHOW_TEXT, doStuff)
+    function doStuff(node) {
+        // Correct common acronyms for accessibility
+        let { textContent } = node
+        if (!textContent.trim() || textContent.length <= 2 || ignore.test(node.parentElement?.tagName)) return
+        // if (!node.nextSibling && !node.previousElementSibling)textContent = textContent.trim().replace(end, dot)
+        if (grammar.ig.test(textContent)) textContent = textContent.replace(grammar.ig, ig)
+        if (grammar.istg.test(textContent)) textContent = textContent.replace(grammar.istg, istg)
+        if (grammar.idk.test(textContent)) textContent = textContent.replace(grammar.idk, idk)
+        if (grammar.ik.test(textContent)) textContent = textContent.replace(grammar.ik, ik)
+        if (grammar.idc.test(textContent)) textContent = textContent.replace(grammar.idc, idc)
+        if (grammar.bc.test(textContent)) textContent = textContent.replace(grammar.bc, because)
+        if (grammar.im.test(textContent)) textContent = textContent.replace(grammar.im, "I'm")
+        if (grammar.i.test(textContent)) textContent = textContent.replace(grammar.i, 'I')
+        if (textContent !== node.textContent) node.textContent = textContent
+    }
+    function idc(str) {
+        let { 0: i, 1: d, 2: c } = str
+        return `${i} ${d === d.toUpperCase() ? "DON'T" : "don't"} ${c === c.toUpperCase() ? 'CARE' : 'care'}`
+    }
+    function idk(str) {
+        let { 0: i, 1: d, 2: k } = str
+        return `${i} ${d === d.toUpperCase() ? "DON'T" : "don't"} ${k === k.toUpperCase() ? 'KNOW' : 'know'}`
+    }
+    function ik(str) {
+        let { 0: i, 1: d, 2: k } = str
+        return `${i} ${k === k.toUpperCase() ? 'KNOW' : 'know'}`
+    }
+    function istg(str) {
+        let { 0: i, 1: s, 2: t, 3: g } = str
+        return `${i} ${s === s.toUpperCase() ? 'SWEAR' : 'swear'} ${t === t.toUpperCase() ? 'TO' : 'to'} ${g === g.toUpperCase() ? 'GOD' : 'god'}`
+    }
+    function because(str) {
+        return `${str[0]}ecause`
+    }
+    function ig(str) {
+        let { 0: i, 1: g } = str
+        return `${i} ${g === g.toUpperCase() ? 'GUESS' : 'guess'}`
+    }
+    // function mutate() { }
     let ran = false
-document.prerendering&&console.log(`Prerendering started for ${location}`)
-if (top === window)
-    // User is viewing file by itself
-    standalone()
-async function standalone() {
-    if (ran) return
-    
-    ran = true
-    let {default: $} = await import('./yay.js'),
-        css = await import('./csshelper.js')
+    // document.prerendering && console.log(`Prerendering started for ${location}`)
+    if (top === window)
+        // User is viewing file by itself
+        standalone()
+    async function standalone() {
+        if (ran) return
+        ran = true
+        let css = await import('./csshelper.js')
         $.body.push($('<div style="place-self:center;margin:10px"><a href="../../diary.html">View in Diary</a></div>'))
-    css.importCSS('../../cute-green.css')
-    css.importCSS(`data:text/css,main{opacity: 1 !important;}`)
-    let main = $.qs('main')
-    main.classList.add('cute-green')
-    main.styles.height='100%'
-    let d = new Date()
-    let today = new Date(location.pathname.slice(9).split('/')[0].replace(/_/g,'/'))
-    let aaaa =`${today.getFullYear()}-${`${today.getMonth()+1}`.padStart(2,0)}-${`${today.getDate()}`.padStart(2,0)}`
-    let input = $(`<input min="2025-04-07" max="${d.getFullYear()}-${`${d.getMonth()+1}`.padStart(2,0)}-${`${d.getDate()}`.padStart(2,0)}" type="date" class="cute-green" value="${aaaa}">`, {
-        events: {
-            async input(){
-                let {0:year,1:month,2:day} = this.value.split('-')
-                if (day[0]==0) day = day[1]
-                if (month[0]==0) month = month[1]
-                let n = await fetch(`../${month}_${day}_${year}/index.html`)
-                if (n.ok) location.assign(`../${month}_${day}_${year}/index.html`)
-                else {
+        css.importCSS('../../cute-green.css')
+        css.importCSS(`data:text/css,main{opacity: 1 !important;}`)
+        let main = $.qs('main')
+        main.classList.add('cute-green')
+        main.styles.height = '100%'
+        let d = new Date()
+        let today = new Date(location.pathname.slice(9).split('/')[0].replace(/_/g, '/'))
+        let aaaa = `${today.getFullYear()}-${`${today.getMonth() + 1}`.padStart(2, 0)}-${`${today.getDate()}`.padStart(2, 0)}`
+        let input = $(`<input min="2025-04-07" max="${d.getFullYear()}-${`${d.getMonth() + 1}`.padStart(2, 0)}-${`${d.getDate()}`.padStart(2, 0)}" type="date" class="cute-green" value="${aaaa}">`, {
+            events: {
+                async input() {
+                    let { 0: year, 1: month, 2: day } = this.value.split('-')
+                    if (day[0] == 0) day = day[1]
+                    if (month[0] == 0) month = month[1]
+                    let n = await fetch(`../${month}_${day}_${year}/index.html`)
+                    if (n.ok) location.assign(`../${month}_${day}_${year}/index.html`)
+                    else {
+                    }
                 }
             }
-        }
-    })
-main.afterbegin=$(`<div style="margin:30px;transform:scale(1.7,1.7);display:flex;justify-content: center"></div>`, null, input)
-    /*
-    let regex = /\//g
-    let style = 'position:fixed;display:none;'
-    let str = location.pathname.slice(9).split('/')[0].replace(/_/g,'/')
-    let day = new Date(str)
-
-    day.setDate(day.getDate() -1)
-    let yesterdayPath = day.toLocaleDateString('en-US').replace(regex, '_')
-    day = new Date(str)
-    day.setDate(day.getDate() + 1)
-    let tomorrowPath =  day.toLocaleDateString('en-US').replace(regex, ' _')
-    let y, t
-    $.body.push(y = $(`<a class="cute-green-button" style="${style};top:-10px" role="button" rel="prev" href="../${yesterdayPath}/index.html">Yesterday</a>`),
-        t = $(`<a class="cute-green-button" style="${style};top:-10px;right:0px" role="button" rel="next" href="../${tomorrowPath}/index.html">Tomorrow</a>`))
-    fetch(`../${yesterdayPath}/index.html`).then(y.show.bind(y,3))
-    fetch(`../${tomorrowPath}/index.html`).then(t.show.bind(t,3))*/
+        })
+        main.afterbegin = $(`<div style="margin:30px;transform:scale(1.7,1.7);display:flex;justify-content: center"></div>`, null, input)
+        /*
+        let regex = /\//g
+        let style = 'position:fixed;display:none;'
+        let str = location.pathname.slice(9).split('/')[0].replace(/_/g,'/')
+        let day = new Date(str)
+    
+        day.setDate(day.getDate() -1)
+        let yesterdayPath = day.toLocaleDateString('en-US').replace(regex, '_')
+        day = new Date(str)
+        day.setDate(day.getDate() + 1)
+        let tomorrowPath =  day.toLocaleDateString('en-US').replace(regex, ' _')
+        let y, t
+        $.body.push(y = $(`<a class="cute-green-button" style="${style};top:-10px" role="button" rel="prev" href="../${yesterdayPath}/index.html">Yesterday</a>`),
+            t = $(`<a class="cute-green-button" style="${style};top:-10px;right:0px" role="button" rel="next" href="../${tomorrowPath}/index.html">Tomorrow</a>`))
+        fetch(`../${yesterdayPath}/index.html`).then(y.show.bind(y,3))
+        fetch(`../${tomorrowPath}/index.html`).then(t.show.bind(t,3))*/
+    }
+    /localhost|127\.0\.0\.1/.test(origin) && console.warn(`%cREMEMBER TO SET WIDTH TO A PERCENTAGE ON ALL <img> ELEMENTS! Count: ${document.images.length}`, 'font-size:1.2rem')
+        ;[].forEach.call(document.links, o => {
+            o.setAttribute('rel', `noopener noreferrer nofollow ${o.getAttribute('rel') || ''}`)
+        })
+        ;[].forEach.call(document.querySelectorAll('img,button[type="image"]'), o => {
+            o = $(o)
+            let hasAlt = 'alt'in o.attr,
+                hasTitle = 'title'in o.attr
+            if (hasAlt && !hasTitle) 
+                o.attr.title = o.attr.alt
+            else if (hasTitle && !hasAlt) 
+                o.attr.alt = o.attr.title
+            else if (!(hasTitle || hasAlt)){
+                o.attr._hidden = 'true' // Screen readers might literally read the src attribute as words
+                console.warn('Missing alt or title attribute:', o.outerHTML)
+            }
+        })
 }
-/localhost|127\.0\.0\.1/.test(origin) && console.warn(`%cREMEMBER TO SET WIDTH TO A PERCENTAGE ON ALL <img> ELEMENTS! Count: ${document.images.length}`, 'font-size:1.2rem')
-;[].forEach.call(document.links, o => {
-    o.setAttribute('rel',`noopener noreferrer nofollow ${o.getAttribute('rel') || ''}`)
+/*
+, {
+    once:true
 })
+*/
+, {timeout: 5000})
