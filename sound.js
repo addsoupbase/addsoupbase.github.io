b3545f3c5e047eb2e4061bc2f9a86a19c7a1c905
addsoupbase <addsoupbase1@gmail.com>
2025-11-28 19:42:35 -0500

    MAJOR memory leak fix
    Ook im just not gonna use the AudioContext API bc its so complicated

--- sound.js
+++ sound.js
@@ -1,55 +1,76 @@
 !function () {
     'use strict'
-    if ('audio' in window) return audio
+    if (window.audio) return audio
+    let globalVolume = 1
     const audio = {
         __proto__: null,
         [Symbol.toStringTag]: 'AudioModule',
         // loaded: new Set,
         set volume(val) {
-            gain.gain.value = val
+            for (let a of sounds.values())
+                a.volume = val
+            globalVolume = val
         },
         get volume() {
-            return gain.gain.value
+            return globalVolume
         }
     }
-    const ctx = Reflect.construct(window.AudioContext || webkitAudioContext, [{
-        latencyHint: 'interactive'
-    }])
-    audio.context = ctx
-    let gain = ctx.createGain()
-    gain.connect(ctx.destination)
+    // const ctx = Reflect.construct(window.AudioContext || webkitAudioContext, [{
+    latencyHint: 'interactive'
+    // }])
+    // audio.context = ctx
+    // let gain = ctx.createGain()
+    // gain.connect(ctx.destination)
     const sounds = new Map
     function load(...sources) {
-        return Promise.all(sources.map(makeBuffer))
+        return Promise.all(sources.map(makeAudio))
     }
     audio.load = load
-    function play(name) {
+    function play(name, overlap = true) {
         let b = sounds.get(name)
-        const source = ctx.createBufferSource()
-        source.buffer = b
-        source.connect(gain).connect(ctx.destination)
-        let out = new Promise(resolve => {
-            source.addEventListener('ended', resolve, { once: true })
+        if (!b) return Promise.resolve(null)
+        if (overlap) {
+            let n = b.cloneNode()
+            n.play()
+            return waitTilFinish(n)
+        }
+        b.play()
+        return waitTilFinish(b)
+    }
+    function waitTilFinish(n) {
+        return new Promise(res => {
+            n.onended = e => {
+                n.onended = null
+                res(e)
+            }
         })
-        source.start()
-        return out
     }
     audio.play = play
-    async function makeBuffer(src) {
-        const b = await fetch(src)
-        const arrayBuffer = await b.arrayBuffer()
-        const buffer = await audio.context.decodeAudioData(arrayBuffer)
-        addToSounds(src, buffer)
-        return buffer
+    async function makeAudio(src) {
+        let n = new Audio(src)
+        function done() {
+            n.oncanplaythrough = n.onerror = null
+            addToSounds(src, n)
+        }
+        return new Promise((res, rej) => {
+            n.oncanplaythrough = () => {
+                done.call(n)
+                res(n)
+            }
+            n.onerror = e => {
+                rej(e)
+                done.call(n)
+            }
+        })
     }
-    function addToSounds(src, buffer) {
+    function addToSounds(src, el) {
         let s = src.split('/').at(-1)
-        sounds.set(s, buffer)
+        sounds.set(s, el)
         // audio.loaded.add(s)
     }
     // function playBackgroundMusic(delay, ...sources) {
 
     // }
-    return Object.defineProperty(constructor.prototype, 'audio', { get() { return audio } }), 
-    Object.freeze(audio)
+    return Object.defineProperty(constructor.prototype, 'audio', { get() { return audio } }),
+        Object.freeze(audio)
 }()
