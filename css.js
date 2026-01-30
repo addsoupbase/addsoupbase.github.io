8e3adbdbec8dab022667703be671d8492e07b347
addsoupbase <addsoupbase1@gmail.com>
2026-01-30 06:34:51 -0500

    fix

--- css.js
+++ css.js
@@ -15,7 +15,6 @@
         addEventListener('error', onerror)
     }
     var sup = CSS.supports,
-        //@dev hasOwn = O.hasOwn,
         imp = /\s*!\s*important\s*$/,
         selCache = new Map,
         isSimple = /^[^)(:]+$/,
@@ -70,16 +69,6 @@
         , fClass = fgeneric.bind(1, ':', pseudoClass),
         fElement = fgeneric.bind(1, '::', pseudoElement),
         batch = ''
-    //@dev ,toValue
-    //@dev, comments = /\/\*[\s\S]*?\*\//g
-    //@dev, parse = /([^{}]+?)(?:;|(\{[\s\S]*?\}))/g
-    //@dev, semicolon = /(?:--)?[-\w]+\s*:(?:(?![^(]*\);|[^"]*";|[^']*';).)*?(?:!\s*important\s*)?(?=;(?![^(]*\)|[^"]*"|[^']*')|\s*$)/g
-    /*if (canWrite && top === self) {
-        // Idk why, but it seems to make the page render faster
-        var p = D.querySelector('p[data-cssid="$$$"]')
-        D.write('<p style="position:absolute !important;transform:scale(0) !important;z-index:-9999 !important;" data-cssid="$$$" aria-hidden="true">.</p>')
-        p = addEventListener('load', p.removeChild.bind(p.parentElement, p), { once: true })
-        }*/
     function dv(prop, val) {
         return vendor(toDash(prop), val)
     }
@@ -108,136 +97,6 @@
             selector = '-' + br[i] + '-' + og
         return s ? a : selector
     }
-    /*@dev
-    var instantiated = false
-    function fromCSS(str) {
-        if (!instantiated) {
-            instantiated = true
-            fromCSS.prototype = O.create(null, {
-                supported: {
-                    get: function () { for (var i in this) if (!sup(i + ':' + this[i])) return false; return true }
-                }, toString: {
-                    value: function () { return toCSS(this) }
-                }, cssFloat: {
-                    get: function () {
-                        return this.float
-                    },
-                    set: function (val) {
-                        this.float = val
-                    }
-                }
-            })
-        }
-        if (!(this instanceof fromCSS)) return new fromCSS(str)
-        if (!toValue) toValue = function (parse) {
-            var proto = {
-                //valueOf: { value: function () { return parseFloat(this.toString()) } } 
-            }
-                // valueOf messes with the + operator >:(
-                , props = O.getOwnPropertyNames(String.prototype)
-            Symbol.iterator && props.push(Symbol.iterator)
-            for (var i = props.length; i--;) {
-                var prop = props[i],
-                    z = ''[prop]
-                typeof z === 'function' && prop !== 'toString' && prop !== 'valueOf' && prop !== 'constructor' && hi(prop, z)
-            }
-            return parse ? a : b
-            function a(p, v) {
-                // indexes are used for the property value, so you must use charAt()/at() to get a character
-                // (or just convert it to a string first)
-                return O.defineProperty(O.defineProperties(parse(p, v.replace(imp, '')), proto), 'length', { value: v.length })
-            }
-            function hi(prop, l) {
-                function value(a, b, c) {
-                    var s = this.toString()
-                    if (prop !== 'concat') switch (l.length) {
-                        case 0: return s[prop]()
-                        case 1: return s[prop](a)
-                        case 2: return s[prop](a, b)
-                        case 3: return s[prop](a, b, c)
-                    }
-                    return Reflect.apply(l, s, arguments)
-                }
-                proto[prop] = {
-                    value: value
-                }
-            }
-            function b(_, v) { return O(v.replace(imp, '')) }
-        }(w.CSSStyleValue && CSSStyleValue.parse)
-        str = str.trim()
-        var a = str.match(semicolon) || str.slice(0, -1).match(semicolon)
-            , o = this,
-        url = /;(?!url\(.*\))/
-        var n = { writable: true, configurable: true, value: null, enumerable: false }
-        if (a) for (var h = a.length; h--;) {
-            var hi = a[h]
-                , strings = hi.split(url).filter(Boolean)
-            for (var i = strings.length; i--;) {
-                var str = strings[i]
-                    , splits = str.split(':')
-                    , b = splits,
-                    l = b[0],
-                    j = b[1],
-                    p = l && l.trim(),
-                    v = j && j.trim()
-                if (p) try {
-                    v = toValue(p, v)
-                    n.value = o[p] = v
-                    if (p.startsWith('--')) O.defineProperty(o, '__' + toCaps(p.substring(2)), n)
-                    else {
-                        var x = toCaps(p)
-                        hasOwn(o, x) || O.defineProperty(o, x, n)
-                    }
-                }
-                catch (e) { if (e.name !== 'TypeError') throw e; o[p] = v }  // Prop unsupported 
-            }
-        }
-    }
-*/
-    /*@dev function fixSheet(me) {
-        if (me === sheet.sheet) return
-        var href = me.href
-        if (href) {
-            var xhr = new XMLHttpRequest
-            xhr.open('GET', href)
-            xhr.onload = function () {
-                load(xhr.responseText)
-            }
-            xhr.send()
-        }
-        else load(me.ownerNode.textContent)
-        function load(text) {
-            if (text.includes('--interactivity')) console.warn("--interactivity does not need to be a custom")
-            var og = text
-            text = text.replace(comments, '')
-            var match
-            while (match = parse.exec(text)) {
-                var rule = match[2]
-                if (rule) {
-                    var r = rule.trim().slice(1, -1).replace(/\n/g, '')
-                        , c = new fromCSS(r)
-                        , s = match[1]
-                    if (/^\s*@(?:supports|media)/.test(s)) continue
-                    var selector = fSelector(s)
-                        , lines = og.split('\n')
-                        , line = href + ':' + (lines.findIndex(function (o) { return o.includes(selector) }) + 1)
-                    if (!O.keys(c).length) console.warn('Empty ruleset:', line)
-                    // i will make this better later
-                    if (href && !sel(selector)) console.warn('Invalid selector:', line)
-                    // auto jump to line
-                    for (var x in c) {
-                        var v = c[x]
-                            , propNotSupported = !sup(x + ':' + 'inherit')
-                            , valNotSupported = !sup(x + ':' + v)
-                        if (propNotSupported || valNotSupported)
-                            console.warn('Invalid ' + (propNotSupported ? 'property ' + x : 'value ' + v) + ':', href + ':' + (lines.findIndex(function (o) { return o.includes(propNotSupported ? x : v) }) + 1))
-                    }
-                    // c.supported || console.warn('One or more rules/properties unsupported', c, sheet.href || sheet.outerHTML)
-                }
-            }
-        }
-    }
-        */
     function fgeneric(d, pseudo, selector) {
         if (sel(selector, true)) return selector
         var matches = selector.match(pseudo)
@@ -270,7 +129,6 @@
             var a = p = p
                 .replace(vendr, '')
             return dr.has(p) ? dr.get(p) : find(p, v) || (
-                // sup(prefix = `-internal-${prop}`, val) ||
                 badCSS("Unrecognized CSS at '" + b(a = p, v + important).slice(1, -1) + "'", _),
                 dr.set(p, a),
                 // Sorry!
@@ -301,11 +159,7 @@
         for (p in o) {
             v = String(o[p])
             p === 'content' && !sup('content', v) && (v = '"' + v + '"')
-            // try {
             str += vendor(toDash(p), v, _) + ':' + v + ';'
-            // } catch (e) {
-            // reportError(e)
-            // }
         }
         return str
     }
@@ -359,36 +213,11 @@
         queueWrite(rules)
         return sheet
     }
-    /*function importCSSSync(src) {
-        // make sure to use blocking=render probably
-        if (src.startsWith('data:text')) {
-            var split = src.split(',')[1]
-            text = split[1]
-            if (split[0].includes('base64,')) text = atob(text)
-            debugger
-        }
-        else {
-            var xhr = new XMLHttpRequest
-            xhr.open('GET', src, false)
-            // xhr.responseType = 'text'
-            xhr.send()
-            var text = xhr.responseText
-        }
-        registerCSSRaw(text, true)
-    }*/
-    /*function importCSS(url) {
-        var n = D.createElement('link')
-        n.rel = 'stylesheet'
-        n.fetchpriority = 'high'
-        n.blocking = 'render'
-        n.href = url
-        return put(n)
-    }*/
     function Sheet() {
         var o = D.getElementById(id)
         if (o) return o
         // if (canWrite) return D.write('<style id="'+id+'" blocking="render">'+str+'</style>'), Sheet()
-        // this branch is slower
+        // ^ this branch is slower
         o = D.createElement('style')
         o.id = id
         o.blocking = 'render'
@@ -414,14 +243,11 @@
     }
     function g(name, iv, inh, sx) {
         var o = '--' + name,
-            key = vendor(name, o = 'var(' + o + ')', true);
-        // try { 
-        (inh ? root : uv)[key] = o
+            key = vendor(name, o = 'var(' + o + ')', true)
+        uv[key] = o
         bulkText += re('--' + name, iv, inh, sx)
         return g
-        // }
-        // catch (e) {e.name === 'InvalidModificationError' || (console.log(o), reportError(e),func || fallback.set(key, vendor(key, 'inherit')))}
-    }
+      }
     function W(s) {
         return ':where(' + s + ')'
     }
@@ -432,17 +258,9 @@
         'scrollbar-color': 'var(--scrollbar-thumb-color) var(--scrollbar-color)',
         'scrollbar-width': 'var(--scrollbar-width)'
     }
-    // performance.mark('css-property-start')
     function lowPriority() {
         //@dev console.time('low priority')
-        // lower priority props
         g("locale", "auto", true, "*")("line-grid", "auto", true, "*")("line-snap", "auto", true, "*")("nbsp-mode", "auto", true, "*")("text-zoom", "auto", false, "*")("line-align", "auto", true, "*")("text-decorations-in-effect", "auto", false, "*")("force-broken-image-icon", "0", false, "<integer>")("float-edge", "content-box", false, "*")("image-region", "auto", true, "*")("box-orient", "inline-axis", false, "*")("box-align", "stretch", false, "*")("box-direction", "normal", false, "*")("box-flex", "0", false, "*")("box-flex-group", "0", false, "*")("box-lines", "single", false, "*")("box-ordinal-group", "1", false, "*")("box-decoration-break", "slice", false, "*")("box-pack", "start", false, "*")("line-clamp", "none", false, "*")("font-smoothing", "auto", true, "*")("mask-position-x", "0%", false, "<length-percentage>")("mask-position-y", "0%", false, "<length-percentage>")("window-dragging", "auto", false, "*")("stack-sizing", "stretch-to-fit", true, "*")("mask-composite", "source-over", false, "*")("window-shadow", "auto", false, "*")("outline-radius", "0 0 0 0", false, "*")("binding", "none", false, "*")("text-blink", "none", false, "*")("image-rect", "auto", true, "*")("content-zoom-limit", "400% 100%", false, "*")("accelerator", "0", false, "*")("context-properties", "none", true, "*")("text-kashida-space", "0%", true, "<percentage>")("interpolation-mode", "none", false, "*")("progress-appearance", "bar", false, "*")("content-zooming", "auto", false, "*")("flow-from", "none", false, "*")("flow-into", "none", false, "*")("content-zoom-chaining", "none", false, "*")("high-contrast-adjust", "auto", true, "*")("touch-select", "grippers", true, "*")("ime-mode", "auto", false, "*")("wrap-through", "wrap", false, "*")("print-color-adjust", "economy", true, "*")("pay-button-style", "white", false, "*")("color-filter", "none", true, "*")("pay-button-type", "plain", false, "*")("visual-effect", "none", true, "*")("text-spacing-trim", "normal", true, "*")("text-group-align", "none", false, "*")("text-autospace", "normal", true, "*")("orient", "inline", false, "*")("ruby-overhang", "auto", true, "*")("max-lines", "none", false, "*")("line-fit-edge", "leading", true, "*")("overflow-scrolling", "auto", false, "*")("column-progression", "auto", false, "*")("dashboard-region", "none", false, "*")("column-axis", "auto", false, "*")("text-size-adjust", "auto", true, "*")("border-vertical-spacing", "auto", false, "*")("buffered-rendering", "auto", false, "*")("behaviour", "url()", false, "<url>")
-        // try {
-        // performance.mark('css-other-start')
-        // ('scrollbar-thumb-color', 'auto', true, '*')
-        // ('scrollbar-color', 'auto', true, '*')
-        // }
-        // catch (e) { reportError(e) }
         //@dev console.timeEnd('low priority')
     }
     function properties() {
@@ -477,8 +295,6 @@
             ('word-wrap', 'normal', false, '*')
         //@dev console.timeEnd('properties')
     }
-    // performance.mark('css-property-end')
-    // ],
     function doRegister() {
         re = function (name, iv, inh, sx) {
             reuse.name = name
@@ -497,7 +313,7 @@
         properties()
     }
     var crisp, stretch, center, matchParent
-    var newName = name
+    , newName = name
     if (!atProperty && CSS.registerProperty) doRegister()
     if (!newName) {
         var align = sup.bind(1, 'text-align')
@@ -565,7 +381,6 @@
         //@dev console.debug(newName.replace(/:where\(([\S\s]*?)\)/g,'$1'))
     }
     var str = name
-    // finally { performance.mark('css-other-end') }
     if (!str) // Selecting all pseudo elements has a massive performance hit
         sn(str = id + '*' + "{" + toCSS(uv, true) + "}" + newName)
     write(str)
@@ -590,19 +405,10 @@
             toDash: toDash,
             vendor: vendor,
             toCSS: toCSS,
-            /*registerProperty: function (name, initialValue, inherits, syntax) {
-                // queueWrite(re(name, initialValue, inherits, syntax))
-            },*/
             formatSelector: fSelector,
-            //@dev fixSheet: fixSheet,
-            //@dev get queue() { return batch },
-            //@dev checkSheets: defer.bind(this, [].forEach.bind(D.styleSheets, fixSheet)),
-            //@devfromCSS: fromCSS,
             onerror: onerror
         })
     y[sym] || O.defineProperty(y, sym, { value: css, enumerable: 1 })
-    // console.debug(performance.measure('css-cache','css-cache-start', 'css-cache-end').toJSON(), performance.measure('css-property', 'css-property-start', 'css-property-end').toJSON())
     return css
 }(!this, self, Symbol.for('[[CSSModule]]'), (self.requestIdleCallback && function (c) { return requestIdleCallback(c, { timeout: 2000 }) }) || (self.scheduler && scheduler.postTask && function (c) { return scheduler.postTask(c, { priority: 'background' }) }) || self.queueMicrotask || self.setImmediate || setTimeout, document, Object, '/*stylesheet auto-generated by css.js*/', constructor.prototype, self.requestAnimationFrame || self.webkitRequestAnimationFrame || self.mozRequestAnimationFrame || self.msRequestAnimationFrame || self.oRequestAnimationFrame))
-//self[Symbol.for('[[CSSModule]]')]
 // ; console.timeEnd('css.js')
