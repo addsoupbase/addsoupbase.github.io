eea00c239b3e66583729ede25701b79adaeffc8b
addsoupbase <addsoupbase1@gmail.com>
2026-03-17 10:30:53 -0400

    it will have to do

--- webcomponents/slide-show.js
+++ webcomponents/slide-show.js
@@ -1,20 +1,11 @@
-/* 
-ok this is the last version i swear (hopefully)
-so this one has something unique that i was TRYING to make for months:
-you can add durations for the individual frames
-i don't think CSS @keyframes can do this!!
-and also it now:
- • correctly transforms from the center
- • doesn't interfere with external transforms
- • can use offset-path correctly now!
-*/
-let svg = document.createRange().createContextualFragment('<svg><animate begin="0s" href="#fe" calcMode=discrete attributeName=x repeatCount="indefinite"/><foreignObject x=-1 width=100 height=100 id="fe"><canvas></canvas></foreignObject></svg>')
+let svg = document.createRange().createContextualFragment('<svg><animate from="0" begin="0s" href="#fe" calcMode=discrete attributeName=x repeatCount="indefinite"/><foreignObject width=100 height=100 id="fe" x=0><canvas></canvas></foreignObject></svg>')
 let bitmaps = new Map
 let sheet = new CSSStyleSheet
-sheet.replaceSync('foreignObject{y:calc(rem(calc(var(--index,0)*(var(--height)/var(--frames-y))),var(--height))*-1px)}svg{contain:paint layout;cursor:pointer;pointer-events:all;transform:translate(-50%,-50%);position:relative}:host{pointer-events:none !important;transform-origin:0 0;display:flex;width:0;height:0;image-rendering:-moz-crisp-edges;image-rendering:-webkit-optimize-contrast;image-rendering:pixelated}')
+/*:host(:state(broken)){width:30px;height:30px;background-color:red;transform:translate(-50%,-50%)}*/
+sheet.replaceSync('foreignObject{y:calc((rem(calc(var(--index,0)*var(--frame-h,0)),var(--height,0))*-1px) - 1px)}svg{contain:paint layout;cursor:pointer;pointer-events:all;transform:translate(-50%,-50%);position:relative}:host{pointer-events:none !important;transform-origin:0 0;display:flex;width:0;height:0;image-rendering:-moz-crisp-edges;image-rendering:-webkit-optimize-contrast;image-rendering:pixelated}')
 let dimensions = new Map
 class SlideShow extends HTMLElement {
-    static observedAttributes = 'values href dur index'.split(' ')
+    static observedAttributes = 'values src dur index'.split(' ')
     static preload(...sources) {
         let out = []
         for (let { framesX = 1, framesY = 1, src, duras } of sources) {
@@ -22,20 +13,44 @@ class SlideShow extends HTMLElement {
             n.src = src
             n.fetchPriority = 'high'
             n.decoding = 'sync'
-            n.onload = () => {
-                let width = n.naturalWidth / framesX
-                let height = n.naturalHeight / framesY
-                duras ??= Array(framesX).fill(1)
-                let url = new URL(src, document.baseURI).toString()
-                dimensions.set(url, [framesX, framesY, width, height])
-                sheet.insertRule(`:host([href="${url}"]){width:${width - 1}px;height:${height - 1}px;}`)
-                out.push(createImageBitmap(n).then(o => {
-                    o.framesX = framesX
-                    o.framesY = framesY
-                    o.duras = duras.map((o, i) => `${(-i * width) - 1}`.repeat(o || 1)).join(';')
-                    bitmaps.set(url, o)
-                }))
-            }
+            out.push(new Promise((resolve, reject) => {
+                n.onerror = reject
+                n.onload = () => {
+                    let width = n.naturalWidth / framesX
+                    let height = n.naturalHeight / framesY
+                    let paddedWidth = width + 2
+                    let paddedHeight = height + 2
+                    duras ??= Array(framesX | 0).fill(1)
+                    let url = new URL(src, document.baseURI).toString()
+                    dimensions.set(url, [framesX, framesY, width, height])
+                    sheet.insertRule(`:host:has(canvas[src="${url}"]){width:${width}px;height:${height}px}`)
+                    let padded = document.createElement('canvas')
+                    padded.width = framesX * paddedWidth
+                    padded.height = framesY * paddedHeight
+                    let ctx = padded.getContext('2d')
+                    ctx.imageSmoothingEnabled = false
+                    for (let row = 0; row < framesY; row++)
+                        // you need to add 1 invisible pixel in each axis because it's broken for some reason!
+                        for (let col = 0; col < framesX; col++)
+                            ctx.drawImage(n, col * width, row * height, width, height, col * paddedWidth + 1, row * paddedHeight + 1, width, height)
+                    createImageBitmap(padded).then(o => {
+                        let total = duras.reduce(add, 0)
+                            , vals = []
+                            , times = []
+                            , acc = 0
+                        duras.forEach((d, i) => {
+                            vals.push(`${(-i * paddedWidth) - 1}`)
+                            times.push(acc / total)
+                            acc += d
+                        })
+                        vals.push(vals[vals.length - 1])
+                        times.push(1)
+                        let out = { bitmap: o, framesX, framesY, paddedWidth, paddedHeight, duras: vals.join(';') }
+                        bitmaps.set(url, out)
+                        resolve(out)
+                    })
+                }
+            }))
         }
         return out
     }
@@ -43,40 +58,42 @@ class SlideShow extends HTMLElement {
         return +this.getAttribute('index') || 0
     }
     set index(index) {
-        this.dispatchEvent(new Event('indexEvent', { bubbles: true, cancelable: true })) && this.setAttribute('index', +index || 0)
-    }
-    connectedCallback() {
-        this.#anim.replaceWith(this.#anim = this.#anim.cloneNode(true))
-        this.time = 0
+        this.setAttribute('index', +index || 0)
     }
     async attributeChangedCallback(attr, oldVal, val) {
         switch (attr) {
             case 'index':
-                this.#fe.style.setProperty('--index', +val)
+                this.dispatchEvent(new Event('indexEvent', { bubbles: true, cancelable: true })) && this.#fe.style.setProperty('--index', +val)
                 break
-            case 'href': {
+            case 'src': {
                 let u = new URL(val, this.baseURI).toString()
-                if (u !== val) return this.setAttribute('href', u)
                 let canvas = this.#sprite
+                canvas.setAttribute('src', u)
                 let ctx = this.#ctx
-                while (!bitmaps.has(val)) await new Promise(requestAnimationFrame)
-                let bitmap = bitmaps.get(val)
+                let b = false
+                ctx.clearRect(0, 0, canvas.width, canvas.height)
+                while (!bitmaps.has(u)) {
+                    b || this.#internals.states.add('broken')
+                    b = true
+                    await new Promise(requestAnimationFrame)
+                }
+                this.#internals.states.delete('broken')
+                let { bitmap, framesX, framesY, paddedHeight, paddedWidth, duras } = bitmaps.get(u)
                 let fe = this.#fe
-                fe.setAttribute('width', canvas.width = bitmap.width)
-                fe.setAttribute('height', canvas.height = bitmap.height)
-                ctx.imageSmoothingEnabled = true
-                ctx.imageSmoothingQuality = 'high'
+                fe.setAttribute('width', canvas.width = framesX * paddedWidth)
+                fe.setAttribute('height', canvas.height = framesY * paddedHeight)
+                ctx.imageSmoothingEnabled = false
                 ctx.drawImage(bitmap, 0, 0)
-                this.#fe.style.setProperty('--height', bitmap.height)
-                this.#fe.style.setProperty('--frames-y', bitmap.framesY)
-                this.#anim.setAttribute('values', bitmap.duras)
+                fe.style.setProperty('--height', framesY * paddedHeight)
+                fe.style.setProperty('--frame-h', paddedHeight)
+                this.#anim.setAttribute('values', duras)
+                break
             }
+            case 'dur':
+                this.#anim.setAttribute(attr, val)
                 break
             default:
                 this.#sprite.setAttribute(attr, val)
-                break
-            case 'dur':
-                this.#anim.setAttribute(attr, val)
                 break
         }
     }
@@ -88,19 +105,27 @@ class SlideShow extends HTMLElement {
     disconnectedCallback() {
         this.pause()
     }
-    // there is a 1px offset and idk how to fix it
+    set dur(val) {
+        this.setAttribute('dur', val)
+    }
+    get dur() {
+        return CSSStyleValue.parse('animation-duration', this.getAttribute('dur'))
+    }
     connectedCallback() {
+        // needed bc it's broken
         let anim = this.#anim
         anim.removeAttribute('href')
-        // needed because for some reason when adding the node to the DOM the animation stops
         requestAnimationFrame(() => { anim.setAttribute('href', '#fe'); this.time = 0; this.resume() })
     }
+    #internals = this.attachInternals()
     constructor() {
         super()
+        this.#internals.states.add('broken')
         let shadow = this.attachShadow({ mode: 'open' })
         shadow.appendChild(svg.cloneNode(true))
         this.#svg = shadow.firstChild
-            ; (this.#anim = shadow.querySelector('animate')).addEventListener('repeatEvent', repeat)
+        this.#anim = shadow.querySelector('animate')
+        this.#anim.addEventListener('repeatEvent', repeat)
         this.#ctx = (this.#sprite = shadow.querySelector('canvas')).getContext('2d')
         this.#fe = shadow.querySelector('foreignObject')
         shadow.adoptedStyleSheets = [sheet]
@@ -122,7 +147,8 @@ class SlideShow extends HTMLElement {
 function repeat(e) {
     e.target.getRootNode().host.dispatchEvent(new Event('repeatEvent', { bubbles: true }))
 }
+function add(a, b) { return a + b }
 customElements.define('slide-show', SlideShow)
 export default SlideShow.preload
-try { await (customElements.whenDefined('slide-show')) } // fix for no top level await
+try { await customElements.whenDefined('slide-show') }
 catch (e) { console.debug(e) }
