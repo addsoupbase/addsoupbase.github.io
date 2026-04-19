d48e9b88f7c51432a3ec2fcff394001944c4379a
addsoupbase <addsoupbase1@gmail.com>
2026-04-19 01:54:41 -0400

    form auto cancel to prevent spam
    also removed redundant module requirement

--- aria.js
+++ aria.js
@@ -1,54 +1,54 @@
-!function (h, d) {
+!function (d) {
     'use strict'
-    h.on(d, {
-        keydown: function (e) {
-            if (e.constructor.name !== 'KeyboardEvent') return console.warn('keydown listener fired was not KeyboardEvent. This is a bug I think! (happens when user clicks the autocomplete thingy, just ignore it)', e)
-            var key = e.key.toLowerCase(), target = e.target, parent = target.parentElement
-            var pressable = parent.role === 'tablist' || target.role === 'button'
-            var repeat = e.repeat
-            switch (key) {
-                case 'arrowleft':
-                case 'arrowright': {
-                    var children = [].slice.call(parent.children)
-                    var index = children.indexOf(document.activeElement)
-                    var sibling
-                    if (key === 'arrowright') sibling = children[(index + 1) % children.length]
-                    else sibling = children[(index - 1 + children.length) % children.length]
-                    if (pressable) sibling.focus()
-                    var all = parent.children.length
-                    while (sibling !== document.activeElement && all--) {
-                        sibling = (key === 'arrowright' ? sibling.nextElementSibling : sibling.previousElementSibling) || (key === 'arrowright' ? parent.firstElementChild : parent.lastElementChild)
-                        sibling.focus()
-                    }
-                    break
+    var n = new Set
+    function submit(e) {
+        if (n.has(this)) e.preventDefault()
+        else n.add(this)
+    }
+    [].forEach.call(document.forms, function (o) {
+        o.addEventListener('submit', submit)
+    })
+    d.addEventListener('keydown', function (e) {
+        if (e.constructor.name !== 'KeyboardEvent') return console.warn('keydown listener fired was not KeyboardEvent. This is a bug I think! (happens when user clicks the autocomplete thingy, just ignore it)', e)
+        var key = e.key.toLowerCase(), target = e.target, parent = target.parentElement
+        var pressable = parent.role === 'tablist' || target.role === 'button'
+        var repeat = e.repeat
+        switch (key) {
+            case 'arrowleft':
+            case 'arrowright': {
+                var children = [].slice.call(parent.children)
+                var index = children.indexOf(document.activeElement)
+                var sibling
+                if (key === 'arrowright') sibling = children[(index + 1) % children.length]
+                else sibling = children[(index - 1 + children.length) % children.length]
+                if (pressable) sibling.focus()
+                var all = parent.children.length
+                while (sibling !== document.activeElement && all--) {
+                    sibling = (key === 'arrowright' ? sibling.nextElementSibling : sibling.previousElementSibling) || (key === 'arrowright' ? parent.firstElementChild : parent.lastElementChild)
+                    sibling.focus()
                 }
-                case ' ':
-                case 'enter':
+                break
+            }
+            case ' ':
+            case 'enter':
+                if (pressable && !repeat) {
+                    e.returnValue = !!(e.preventDefault && e.preventDefault())
+                    target.click()
+                }
+                break
+            case 'home':
+            case 'end': {
+                var all = parent.children.length
+                do {
+                    var toggle = key === 'home' ? (toggle ? toggle.nextElementSibling : parent.firstElementChild) : (toggle ? toggle.previousElementSibling : parent.lastElementChild)
                     if (pressable && !repeat) {
-                        e.returnValue = !!(e.preventDefault && e.preventDefault())
-                        target.click()
+                        toggle.focus()
                     }
-                    break
-                case 'home':
-                case 'end': {
-                    var all = parent.children.length
-                    do {
-                        var toggle = key === 'home' ? (toggle ? toggle.nextElementSibling : parent.firstElementChild) : (toggle ? toggle.previousElementSibling : parent.lastElementChild)
-                        if (pressable && !repeat) {
-                            toggle.focus()
-                        }
-                    }
-                    while (document.activeElement !== toggle && all--)
-                    toggle && toggle.click()
-                    break
                 }
+                while (document.activeElement !== toggle && all--)
+                toggle && toggle.click()
+                break
             }
         }
-    })
-    /*@dev
-    var headings = d.getElementsByTagName('h1')
-        if (headings.length > 1) {
-        console.dirxml.apply(console, headings)
-        }
-    */
-}(window[Symbol.for("[[HModule]]")], document)
+    }, true)
+}(document)
