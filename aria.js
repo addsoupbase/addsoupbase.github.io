82c13c83ed5388d3387431fcdbec709e06a2f791
addsoupbase <addsoupbase1@gmail.com>
2026-04-19 18:53:12 -0400

    fix

--- aria.js
+++ aria.js
@@ -1,29 +1,38 @@
 !function (d) {
     'use strict'
     var n = new Set
-    function submit(e) {
-        if (n.has(this)) e.preventDefault()
-        else n.add(this)
-    }
-    [].forEach.call(document.forms, function (o) {
-        o.addEventListener('submit', submit)
-    })
+    d.addEventListener('submit', function (e) {
+        // just to prevent form spam
+        var t = e.target
+        if (n.has(t)) e.preventDefault()
+        else n.add(t)
+    }, true)
+    var ignore = [
+        "text",
+        "email",
+        "number",
+        "search",
+        "tel",
+        "url",
+        "password"
+    ].map(function (o) { return 'input[type="' + o + '"]' }).join(',')
     d.addEventListener('keydown', function (e) {
         if (e.constructor.name !== 'KeyboardEvent') return console.warn('keydown listener fired was not KeyboardEvent. This is a bug I think! (happens when user clicks the autocomplete thingy, just ignore it)', e)
         var key = e.key.toLowerCase(), target = e.target, parent = target.parentElement
+        if ((target.tagName === 'input' && !target.hasAttribute('type')) || [].includes.call(d.querySelectorAll(ignore), target)) return
         var pressable = parent.role === 'tablist' || target.role === 'button'
-        var repeat = e.repeat
+            , repeat = e.repeat
         switch (key) {
             case 'arrowleft':
             case 'arrowright': {
                 var children = [].slice.call(parent.children)
-                var index = children.indexOf(document.activeElement)
+                var index = children.indexOf(d.activeElement)
                 var sibling
                 if (key === 'arrowright') sibling = children[(index + 1) % children.length]
                 else sibling = children[(index - 1 + children.length) % children.length]
                 if (pressable) sibling.focus()
                 var all = parent.children.length
-                while (sibling !== document.activeElement && all--) {
+                while (sibling !== d.activeElement && all--) {
                     sibling = (key === 'arrowright' ? sibling.nextElementSibling : sibling.previousElementSibling) || (key === 'arrowright' ? parent.firstElementChild : parent.lastElementChild)
                     sibling.focus()
                 }
@@ -45,7 +54,7 @@
                         toggle.focus()
                     }
                 }
-                while (document.activeElement !== toggle && all--)
+                while (d.activeElement !== toggle && all--)
                 toggle && toggle.click()
                 break
             }
